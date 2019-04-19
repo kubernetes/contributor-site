16de61fbfc94c69073030139b830b05dbad67cd0
lucperkins <lucperkins@gmail.com>
2019-04-19 11:35:25 -0700

    Rework file naming
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,9 +2,11 @@ function renderCalendar() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    timeZone: 'local',
-    header: { center: 'dayGridMonth,timeGridWeek' },
-    plugins: [ 'dayGrid' ]
+    plugins: [ 'dayGrid', 'googleCalendar' ],
+    googleCalendarApiKey: 'AIzaSyDn_UhFPLDgxouI5nc8hOULFY25EjwGR44',
+    events: {
+      googleCalendarId: 'cgnt364vd8s86hr2phapfjc6uk@group.calendar.google.com'
+    }
   });
 
   calendar.render();
