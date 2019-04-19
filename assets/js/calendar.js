3f6a6cb98006ac1dbd9fc018d783650865947a56
lucperkins <lucperkins@gmail.com>
2019-04-19 13:25:28 -0700

    Remove unused CSS and JS files
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -7,7 +7,7 @@ function renderCalendar() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid', 'list', 'googleCalendar', 'moment' ],
+    plugins: [ 'list', 'googleCalendar', 'moment' ],
     googleCalendarApiKey: 'AIzaSyDn_UhFPLDgxouI5nc8hOULFY25EjwGR44',
     events: {
       googleCalendarId: 'cgnt364vd8s86hr2phapfjc6uk@group.calendar.google.com'
