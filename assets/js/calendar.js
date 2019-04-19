a0ccc8392df59e8f7fbcbc992d2b2b88791773d9
lucperkins <lucperkins@gmail.com>
2019-04-19 13:06:46 -0700

    Switch to list view
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,17 +1,19 @@
+function openEvent(event) {
+  window.open(event.url, 'gcalevent', 'width=600,height=600');
+  return false;
+}
+
 function renderCalendar() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid', 'googleCalendar', 'moment' ],
+    plugins: [ 'dayGrid', 'list', 'googleCalendar', 'moment' ],
     googleCalendarApiKey: 'AIzaSyDn_UhFPLDgxouI5nc8hOULFY25EjwGR44',
     events: {
       googleCalendarId: 'cgnt364vd8s86hr2phapfjc6uk@group.calendar.google.com'
     },
-    eventClick: function(event) {
-      window.open(event.url, 'gcalevent', 'width=600,height=600');
-      return false;
-    },
-    defaultView: 'dayGridWeek',
+    eventClick: openEvent,
+    defaultView: 'listWeek',
     timezone: 'local',
     displayEventTime: true,
     eventLimit: true,
