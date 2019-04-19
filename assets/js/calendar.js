578f7f7a9030be96e732943fb1f3e36647c3b74b
lucperkins <lucperkins@gmail.com>
2019-04-19 11:46:34 -0700

    Finish calendar configuration
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,10 +2,29 @@ function renderCalendar() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid', 'googleCalendar' ],
+    plugins: [ 'dayGrid', 'googleCalendar', 'moment' ],
     googleCalendarApiKey: 'AIzaSyDn_UhFPLDgxouI5nc8hOULFY25EjwGR44',
     events: {
       googleCalendarId: 'cgnt364vd8s86hr2phapfjc6uk@group.calendar.google.com'
+    },
+    eventClick: function(event) {
+      window.open(event.url, 'gcalevent', 'width=600,height=600');
+      return false;
+    },
+    defaultView: 'dayGridWeek',
+    timezone: 'local',
+    displayEventTime: true,
+    eventLimit: true,
+    navLinks: true,
+    header: {
+      left: 'prev,today,next',
+      center: 'title',
+      right: 'agendaDay,agendaWeek,month'
+    },
+    views: {
+      month: {
+        eventLimit: 6
+      }
     }
   });
 
