97520acd59a56f58ceac380b770e3e190d2be590
vishakha <vishakhanihore.10@gmail.com>
2020-01-13 00:30:52 +0530

    feat: Making events page into a scheduler and merging with calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -7,22 +7,20 @@ function renderCalendar() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'list', 'googleCalendar', 'moment' ],
-    googleCalendarApiKey: 'AIzaSyDlAwor0K8xljdp_r3V-9nFKnyoBQzm3Ro',
-    events: {
-      googleCalendarId: 'cgnt364vd8s86hr2phapfjc6uk@group.calendar.google.com'
+    plugins: [ 'dayGrid', 'googleCalendar', 'moment' ],
+    defaultView: 'dayGridMonth',
+    googleCalendarApiKey: 'AIzaSyCePJ8tLdqZTHQ71_17eSCREAh4YXWRVDI',
+    events: 'vishakhanihore.10@gmail.com',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,listYear'
     },
     eventClick: openEvent,
-    defaultView: 'listWeek',
     timezone: 'local',
     displayEventTime: true,
     eventLimit: true,
     navLinks: true,
-    header: {
-      left: 'prev,today,next',
-      center: 'title',
-      right: 'agendaDay,agendaWeek,month'
-    },
     views: {
       month: {
         eventLimit: 6
