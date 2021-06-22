b15e84e66f9e28f3095753ed655b45c87f959b93
RinkiyaKeDad <arshsharma461@gmail.com>
2021-06-22 18:11:28 +0530

    basic copying working
    Signed-off-by: RinkiyaKeDad <arshsharma461@gmail.com>

--- hack/gen_content.go
+++ hack/gen_content.go
@@ -1,28 +1,90 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"strconv"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	// TODO: replace this with reading from env vars
-	repos := []string{"https://github.com/kubernetes/community.git", "https://github.com/kubernetes/sig-release.git"}
+type Info struct {
+	Sources []struct {
+		Repo  string `yaml:"repo"`
+		Files []struct {
+			Src  string `yaml:"src"`
+			Dest string `yaml:"dest"`
+		} `yaml:"files"`
+	} `yaml:"sources"`
+}
 
-	err := os.Mkdir("./_tmp", 0755)
+func main() {
+	filename, _ := filepath.Abs("./info.yaml")
+	yamlFile, err := ioutil.ReadFile(filename)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i, r := range repos {
-		// TODO: concatenate repo name instead
-		concatenatedPath := "./_tmp/" + strconv.Itoa(i)
-		gitClone := exec.Command("git", "clone", r, concatenatedPath)
+	var info Info
+	err = yaml.Unmarshal(yamlFile, &info)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Mkdir("./_tmp", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Mkdir("./_out", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, source := range info.Sources {
+		repo := source.Repo
+		// TODO: replace hard coded "community" with some variable
+		concatenatedPath := "./_tmp/" + "community"
+		gitClone := exec.Command("git", "clone", repo, concatenatedPath)
 		err := gitClone.Run()
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		for _, file := range source.Files {
+			copyFrom := concatenatedPath + file.Src
+			copyTo := "./_out" + file.Dest
+			Copy(copyFrom, copyTo)
+		}
 	}
+
+}
+
+func Copy(src, dst string) error {
+	input, err := ioutil.ReadFile(src)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	dir, _ := filepath.Split(dst)
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = os.Create(dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(dst, input, 0644)
+	if err != nil {
+		fmt.Println("Error creating", dst)
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
