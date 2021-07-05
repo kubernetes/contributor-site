7879860d987a9f0b3c9ad5bafe98fef4840f69a6
RinkiyaKeDad <arshsharma461@gmail.com>
2021-07-05 17:48:05 +0530

    a bit more to go
    Signed-off-by: RinkiyaKeDad <arshsharma461@gmail.com>

--- hack/gen_content.go
+++ hack/gen_content.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -51,7 +52,8 @@ func main() {
 	for _, source := range info.Sources {
 		repo := source.Repo
 		// TODO: replace hard coded "community" with some variable
-		concatenatedPath := "./_tmp/" + "community"
+		folderName, _ := GetStringInBetweenTwoString(repo, "https://github.com/kubernetes/", ".git")
+		concatenatedPath := "./_tmp/" + folderName
 		gitClone := exec.Command("git", "clone", repo, concatenatedPath)
 		err := gitClone.Run()
 		if err != nil {
@@ -60,7 +62,7 @@ func main() {
 
 		for _, file := range source.Files {
 			copyFrom := concatenatedPath + file.Src
-			copyTo := "./_out" + file.Dest
+			copyTo := "./_out/" + folderName + file.Dest
 			Copy(copyFrom, copyTo)
 		}
 	}
@@ -74,7 +76,8 @@ func Copy(src, dst string) error {
 		return err
 	}
 
-	fmt.Println(GetAllLinks(string(input)))
+	mkdown, mp := GetAllLinks(string(input))
+	PrettyPrint(mp)
 
 	dir, _ := filepath.Split(dst)
 	err = os.MkdirAll(dir, os.ModePerm)
@@ -85,7 +88,7 @@ func Copy(src, dst string) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = ioutil.WriteFile(dst, input, 0644)
+	err = ioutil.WriteFile(dst, []byte(mkdown), 0644)
 	if err != nil {
 		fmt.Println("Error creating", dst)
 		fmt.Println(err)
@@ -94,33 +97,63 @@ func Copy(src, dst string) error {
 	return nil
 }
 
-func GetAllLinks(markdown string) map[string]string {
+func GetAllLinks(markdown string) (string, map[string]string) {
 	// Holds all the links and their corresponding values
 	m := make(map[string]string)
 
 	// Regex to extract link and text attached to link
 	re := regexp.MustCompile(`\[([^\]]*)\]\(([^)]*)\)`)
+	re2 := regexp.MustCompile(`\[([^\]]*)\]\:\ ([^ ]*)\w`)
 
 	scanner := bufio.NewScanner(strings.NewReader(markdown))
 	stop := false
 	// Scans line by line
 	for scanner.Scan() {
-		if strings.HasPrefix(scanner.Text(), "```") == true {
+		if strings.HasPrefix(scanner.Text(), "```") {
 			stop = !stop
 		}
 
-		if stop == false {
+		if !stop {
 			// Make regex
 			matches := re.FindAllStringSubmatch(scanner.Text(), -1)
-
+			matches2 := re2.FindAllStringSubmatch(scanner.Text(), -1)
 			// Only apply regex if there are links and the link does not start with #
 			if matches != nil {
-				if strings.HasPrefix(matches[0][2], "#") == false {
-					// fmt.Println(matches[0][2])
+				if !strings.HasPrefix(matches[0][2], "#") {
+					markdown = strings.Replace(markdown, matches[0][2], "hugo-url", -1)
 					m[matches[0][1]] = matches[0][2]
+				}
+			}
+			if matches2 != nil {
+				if !strings.HasPrefix(matches2[0][2], "#") {
+					markdown = strings.Replace(markdown, matches2[0][2], "hugo-url", -1)
+					m[matches2[0][1]] = matches2[0][2]
 				}
 			}
 		}
 	}
-	return m
+	fmt.Println(markdown)
+	return markdown, m
+}
+
+func GetStringInBetweenTwoString(str string, startS string, endS string) (result string, found bool) {
+	s := strings.Index(str, startS)
+	if s == -1 {
+		return result, false
+	}
+	newS := str[s+len(startS):]
+	e := strings.Index(newS, endS)
+	if e == -1 {
+		return result, false
+	}
+	result = newS[:e]
+	return result, true
+}
+
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return
 }
